608a589edc12db31eb7d89a4ff9951a34bd164a0
Aleksandr Levchuk <alevchuk@gmail.com>
2011-08-20 14:23:07 -0700

    Move mark_{start,done} to a separate file in api/

--- api/pipeline.sh
+++ api/pipeline.sh
@@ -1,26 +1,41 @@
+set -e
+set -u
+
+ALWAYS_RUN=false # Default should be true for portability
 TIMEFORMAT="%0lR
 " # When timing, print no-fraction, long-format, elapsed time, and a linebreak
 
 function pipeline {
 
-  prefix=$1       # Location for the time-stamp (*-done and *-inprogress) files
-  parrent_done=$2 # Location(s) for the upstream time-stamp files
+  parent_prefix=$1  # Location(s) for the upstream time-stamp files
+  prefix=$2         # Location for time-stamp files *-done and *-inprogress
+  caption=$3
+  payload=$4        # What needs to be done? (a callback function)
 
-  in_progress="$prefix-inprogress-or-failed"
-  done_time="$prefix-done"
-  if $ALWAYS_RUN || [ ! -f $done_time ] || [ $parrent_done -nt $done_time ]
+  echo $caption 2> /dev/stderr
+
+  in_progress=$prefix-inprogress-or-failed
+  done_time=$prefix-done
+  parent_time=$parent_prefix-done
+
+  if ! $ALWAYS_RUN && [ ! -f $parent_time ]; then
+    echo "ERROR: Parent done file does not exist: $parent_time" > /dev/stderr
+    exit 1
+  fi
+
+  if $ALWAYS_RUN || [ ! -f $done_time ] || [ $parent_time -nt $done_time ]
   then
     time (
       mv $done_time $in_progress 2> /dev/null && true
       touch $in_progress
   
-      payload
+      $payload
   
       mv $in_progress $done_time
       touch $done_time
     )
   else
-    echo "Already done." 2> /dev/stderr
+    printf "Already done.\n\n" 2> /dev/stderr
   fi
 
 }
